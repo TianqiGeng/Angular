a1807d33c03bdb316526c9c7138d87a98c47d889
Tian Qi Geng <tqgeng@cn.ibm.com>
2019-12-04 20:00:31 +0800

    3-7

--- pinduoduolesson/pinduoduo/src/app/app.module.ts
+++ pinduoduolesson/pinduoduo/src/app/app.module.ts
@@ -2,19 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ScrollableTabComponent, ImageSliderComponent } from './component';
-
+import { ScrollableTabComponent, ImageSliderComponent, HorizontalGridComponent } from './component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ScrollableTabComponent,
     ImageSliderComponent,
+    HorizontalGridComponent,
+
 
   ],
   imports: [
     BrowserModule,
-
+    FormsModule
 
   ],
   providers: [],
